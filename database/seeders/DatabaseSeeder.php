4f00f73b553815067e3501c3971978dae6b1b8f4
Muhammad Athar <athar957@gmail.com>
2023-09-14 11:52:20 +0500

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\Test::factory(10)->create();
+        
+    }
+}
